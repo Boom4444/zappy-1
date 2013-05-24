7140cde15653fd04c76d835f820d3de2f19114f0
ivanignatiev <ignatiev.ivan@gmail.com>
2013-05-24 19:48:02 +0200

    begin client ai

---
+++ client_ai.c
@@ -0,0 +1,19 @@
+/*
+** client_ai.c for zappy in /home/ignatiev/Projects/zappy
+** 
+** Made by ivan ignatiev
+** Login   <ignati_i@epitech.net>
+** 
+** Started on  Fri May 24 19:45:50 2013 ivan ignatiev
+** Last update Fri May 24 19:47:05 2013 ivan ignatiev
+*/
+
+#include    <stdlib.h>
+#include    <stdio.h>
+
+int         main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    return (EXIT_SUCCESS);
+}
